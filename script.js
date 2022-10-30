20762718d913945e01ff4da86ef7657a9dfbfc23
Cgotts <cobi.gottschalk@gmail.com>
2022-10-29 18:04:05 -0700

    Finished adding main elements to html and pairing it with css

--- script.js
+++ script.js
@@ -2,28 +2,22 @@ var displayCurrentTime = document.querySelector('#currentDay')
 var hourlySchedule = document.querySelector('.container')
 var time = moment()
 
+var nine = document.querySelector("#nine")
+var ten =
+
+
+
+
+
+
 
 
 setInterval(function (){
 
 var time = moment().format("dddd MMM Do, YYYY")
-displayCurrentTime.textContent = time
-
-
+displayCurrentTime.textContent = time             // this worked because it wasnt jquery selected like in the example video
 
 }, 1000);
 
 
-// .format('YYYY-MM-DD [at] hh:mm:ss a')
 
-
-
-
-// setInterval(function (){
-
-//     var time = moment().format('YYYY MM Do')
-//     hourlySchedule.textContent = time
-    
-    
-                                                          // this worked because it wasnt jquery selected like in the example video
-//     }, 1000);
